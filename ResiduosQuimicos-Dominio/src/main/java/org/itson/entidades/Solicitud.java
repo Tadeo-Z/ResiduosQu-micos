44569395c0ddbbf41d15f3a38ed3379333f82ec1
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:33:34 -0600

    Solicitud.java

--- ResiduosQuimicos-Dominio/src/main/java/org/itson/entidades/Solicitud.java
+++ ResiduosQuimicos-Dominio/src/main/java/org/itson/entidades/Solicitud.java
@@ -20,59 +20,113 @@ import javax.persistence.TemporalType;
 /**
  *
  * @author TADEO
+ *
+ * Clase que representa la entidad de Solicitud.
+ * 
+ * La entidad está mapeada a una tabla llamada "solicitudes" en la base de datos.
  */
 @Entity
 @Table(name = "solicitudes")
 public class Solicitud implements Serializable {
 
+    /** Identificador único de la solicitud. */
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /** Fecha en que se realiza la solicitud. */
     @Column(name = "fechaSolicitud", nullable = false)
     @Temporal(TemporalType.DATE)
     private Calendar fechaSolicitud;
     
+    /** Relación One-to-Many con la entidad ResiduoSolicitud. */
     @OneToMany(mappedBy = "solicitud")
     private List<ResiduoSolicitud> residuoSolicitud;
 
+    /** Constructor por defecto. */
     public Solicitud() {
     }
 
+    /**
+     * Constructor que toma un ID como parámetro.
+     * 
+     * @param id El identificador único de la solicitud.
+     */
     public Solicitud(Long id) {
         this.id = id;
     }
 
+    /**
+     * Constructor que toma la fecha de solicitud y una lista de ResiduoSolicitud como parámetros.
+     * 
+     * @param fechaSolicitud    La fecha en que se realiza la solicitud.
+     * @param residuoSolicitud  La lista de relaciones entre residuos y la solicitud.
+     */
     public Solicitud(Calendar fechaSolicitud, List<ResiduoSolicitud> residuoSolicitud) {
         this.fechaSolicitud = fechaSolicitud;
         this.residuoSolicitud = residuoSolicitud;
     }
     
+    /**
+     * Obtiene el identificador único de la solicitud.
+     * 
+     * @return El ID de la solicitud.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Establece el identificador único de la solicitud.
+     * 
+     * @param id El nuevo ID de la solicitud.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Obtiene la fecha en que se realiza la solicitud.
+     * 
+     * @return La fecha de solicitud.
+     */
     public Calendar getFechaSolicitud() {
         return fechaSolicitud;
     }
 
+    /**
+     * Establece la fecha en que se realiza la solicitud.
+     * 
+     * @param fechaSolicitud La nueva fecha de solicitud.
+     */
     public void setFechaSolicitud(Calendar fechaSolicitud) {
         this.fechaSolicitud = fechaSolicitud;
     }
 
+    /**
+     * Obtiene la lista de relaciones ResiduoSolicitud asociadas a la solicitud.
+     * 
+     * @return La lista de ResiduoSolicitud.
+     */
     public List<ResiduoSolicitud> getResiduoSolicitud() {
         return residuoSolicitud;
     }
 
+    /**
+     * Establece la lista de relaciones ResiduoSolicitud asociadas a la solicitud.
+     * 
+     * @param residuoSolicitud La nueva lista de ResiduoSolicitud.
+     */
     public void setResiduoSolicitud(List<ResiduoSolicitud> residuoSolicitud) {
         this.residuoSolicitud = residuoSolicitud;
     }
     
+    /**
+     * Genera un código hash único para la solicitud.
+     * 
+     * @return El código hash.
+     */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -80,22 +134,33 @@ public class Solicitud implements Serializable {
         return hash;
     }
 
+    /**
+     * Compara si dos solicitudes son iguales.
+     * 
+     * @param object El objeto a comparar.
+     * @return `true` si las solicitudes son iguales, `false` en caso contrario.
+     */
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Solicitud)) {
             return false;
         }
         Solicitud other = (Solicitud) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+        return (this.id == null && other.id == null) || (this.id != null && this.id.equals(other.id));
     }
 
+    /**
+     * Obtiene una representación en cadena de la solicitud.
+     * 
+     * @return La representación en cadena.
+     */
     @Override
     public String toString() {
-        return "Solicitud{" + "id=" + id + ", fechaSolicitud=" + fechaSolicitud + ", residuoSolicitud=" + residuoSolicitud + '}';
+        return "Solicitud{" +
+               "id=" + id +
+               ", fechaSolicitud=" + fechaSolicitud +
+               ", residuoSolicitud=" + residuoSolicitud +
+               '}';
     }
     
 }
