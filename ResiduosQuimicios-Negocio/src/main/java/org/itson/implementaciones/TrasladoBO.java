63537af24ef424dad4302d0710ed78d10d141194
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:13:17 -0600

    TrasladoBO.java

--- ResiduosQuimicios-Negocio/src/main/java/org/itson/implementaciones/TrasladoBO.java
+++ ResiduosQuimicios-Negocio/src/main/java/org/itson/implementaciones/TrasladoBO.java
@@ -13,45 +13,100 @@ import org.itson.interfaces.iTrasladoBO;
 /**
  *
  * @author TADEO
+ *
+ * Clase que representa la lógica de negocio (BO) para la gestión de traslados.
+ * Implementa la interfaz iTrasladoBO.
+ * 
+ * El propósito de esta clase es proporcionar métodos para registrar y buscar información
+ * relacionada con traslados, interactuando con la capa de persistencia.
+ * 
+ * El objeto FachadaPersistencia se utiliza para la interacción con la capa de persistencia.
+ * Se manejan excepciones específicas (BOException) para encapsular posibles errores durante
+ * las operaciones de negocio.
  */
-public class TrasladoBO implements iTrasladoBO{
+public class TrasladoBO implements iTrasladoBO {
 
+    /**
+     * Objeto de la clase FachadaPersistencia utilizado para interactuar con la capa de persistencia.
+     */
     FachadaPersistencia persistencia;
-    
-    public TrasladoBO(){
+
+    /**
+     * Constructor predeterminado que inicializa el objeto FachadaPersistencia.
+     */
+    public TrasladoBO() {
         persistencia = new FachadaPersistencia();
     }
-    
+
+    /**
+     * Registra un nuevo traslado.
+     * 
+     * @param trasladoRegistrar El Traslado que se registrará.
+     * @return El Traslado registrado.
+     * @throws BOException Si hay un problema durante el proceso de registro, encapsulando
+     *                     tanto PersistenciaException como ValidacionesException.
+     */
     @Override
     public Traslado registrar(Traslado trasladoRegistrar) throws BOException {
-        try{
+        try {
+            // Valida el Traslado antes del registro
             this.validarRegistrarTraslado(trasladoRegistrar);
+            
+            // Registra el Traslado utilizando la capa de persistencia
             return persistencia.registrar(trasladoRegistrar);
-        }catch(PersistenciaException e){
+        } catch (PersistenciaException e) {
+            // Propaga PersistenciaException como BOException
             throw new BOException(e.getMessage(), e);
-        }catch(ValidacionesException a){
+        } catch (ValidacionesException a) {
+            // Propaga ValidacionesException como BOException
             throw new BOException(a.getMessage(), a);
         }
     }
-    
-    public Traslado buscar(int idTraslado) throws BOException{
-        try{
+
+    /**
+     * Busca un traslado por su ID.
+     * 
+     * @param idTraslado El ID del traslado a buscar.
+     * @return El Traslado encontrado.
+     * @throws BOException Si hay un problema durante el proceso de búsqueda, encapsulando
+     *                     tanto PersistenciaException como ValidacionesException.
+     */
+    public Traslado buscar(int idTraslado) throws BOException {
+        try {
+            // Valida el ID antes de buscar el Traslado
             this.validarBuscarTraslado(idTraslado);
+            
+            // Busca el Traslado utilizando la capa de persistencia
             return persistencia.buscarTraslado(idTraslado);
-        }catch(PersistenciaException | ValidacionesException e){
+        } catch (PersistenciaException | ValidacionesException e) {
+            // Propaga PersistenciaException o ValidacionesException como BOException
             throw new BOException(e.getMessage(), e);
         }
     }
-    
-    private void validarRegistrarTraslado(Traslado trasladoValidar) throws ValidacionesException{
-        if(trasladoValidar == null){
-            throw new ValidacionesException("Traslado a registrar es nulo / No hay informacion");
+
+    /**
+     * Valida el Traslado antes del registro.
+     * 
+     * @param trasladoValidar El Traslado a validar.
+     * @throws ValidacionesException Si la validación falla, indicando que el Traslado
+     *                              es nulo o carece de información necesaria.
+     */
+    private void validarRegistrarTraslado(Traslado trasladoValidar) throws ValidacionesException {
+        if (trasladoValidar == null) {
+            throw new ValidacionesException("Traslado a registrar es nulo / No hay información");
         }
     }
-    
-    private void validarBuscarTraslado(int trasladoValidar) throws ValidacionesException{
-        if(trasladoValidar <= 0){
+
+    /**
+     * Valida el ID del Traslado antes de la búsqueda.
+     * 
+     * @param trasladoValidar El ID del Traslado a validar.
+     * @throws ValidacionesException Si la validación falla, indicando que el ID es menor o igual a 0.
+     */
+    private void validarBuscarTraslado(int trasladoValidar) throws ValidacionesException {
+        if (trasladoValidar <= 0) {
             throw new ValidacionesException("ID de traslado es menor o igual a 0");
         }
     }
 }
+
