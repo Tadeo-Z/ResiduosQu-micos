be321a3cb26915eba824789d60619f536def5c15
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 08:55:52 -0600

    ValidacionesException.java

--- ResiduosQuimicios-Negocio/src/main/java/org/itson/excepciones/ValidacionesException.java
+++ ResiduosQuimicios-Negocio/src/main/java/org/itson/excepciones/ValidacionesException.java
@@ -8,15 +8,33 @@ package org.itson.excepciones;
  *
  * @author TADEO
  */
-public class ValidacionesException extends Exception{
+/**
+ * Excepción personalizada para manejar errores relacionados con validaciones.
+ * Puede ser utilizada para representar situaciones donde las validaciones de datos fallan.
+ */
+public class ValidacionesException extends Exception {
 
+    /**
+     * Constructor predeterminado de la excepción sin mensaje.
+     */
     public ValidacionesException() {
     }
 
+    /**
+     * Constructor que acepta un mensaje descriptivo para la excepción.
+     *
+     * @param message Mensaje descriptivo de la excepción.
+     */
     public ValidacionesException(String message) {
         super(message);
     }
 
+    /**
+     * Constructor que acepta un mensaje descriptivo y una causa subyacente para la excepción.
+     *
+     * @param message Mensaje descriptivo de la excepción.
+     * @param cause Causa subyacente de la excepción.
+     */
     public ValidacionesException(String message, Throwable cause) {
         super(message, cause);
     }
