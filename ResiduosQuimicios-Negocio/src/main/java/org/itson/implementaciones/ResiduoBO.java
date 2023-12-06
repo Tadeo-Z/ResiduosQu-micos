c8fc0eb6bdbbf5e61dd96945d58538af65f9b6ac
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:07:41 -0600

    ResiduoBO.java

--- ResiduosQuimicios-Negocio/src/main/java/org/itson/implementaciones/ResiduoBO.java
+++ ResiduosQuimicios-Negocio/src/main/java/org/itson/implementaciones/ResiduoBO.java
@@ -13,44 +13,101 @@ import org.itson.interfaces.iResiduoBO;
 /**
  *
  * @author TADEO
+ *
+ *
+ * Clase que representa la lógica de negocio (BO) para la gestión de residuos.
+ * Implementa la interfaz iResiduoBO.
+ * 
+ * El propósito de esta clase es proporcionar métodos para registrar y buscar información
+ * relacionada con residuos, interactuando con la capa de persistencia.
+ * 
+ * El objeto FachadaPersistencia se utiliza para la interacción con la capa de persistencia.
+ * Se manejan excepciones específicas (BOException) para encapsular posibles errores durante
+ * las operaciones de negocio.
+ * 
+ * Autor: TADEO
  */
-public class ResiduoBO implements iResiduoBO{
+public class ResiduoBO implements iResiduoBO {
     
+    /**
+     * Objeto de la clase FachadaPersistencia utilizado para interactuar con la capa de persistencia.
+     */
     FachadaPersistencia persistencia;
 
-    public ResiduoBO(){
+    /**
+     * Constructor predeterminado que inicializa el objeto FachadaPersistencia.
+     */
+    public ResiduoBO() {
         persistencia = new FachadaPersistencia();
     }
-    
+
+    /**
+     * Registra un nuevo residuo.
+     * 
+     * @param residuoRegistrar El objeto Residuo que se registrará.
+     * @return El Residuo registrado.
+     * @throws BOException Si hay un problema durante el proceso de registro, encapsulando
+     *                     tanto PersistenciaException como ValidacionesException.
+     */
     @Override
     public Residuo registrar(Residuo residuoRegistrar) throws BOException {
-        try{
+        try {
+            // Valida el Residuo antes del registro
             this.validarRegistrarResiduo(residuoRegistrar);
+            
+            // Registra el Residuo utilizando la capa de persistencia
             return persistencia.registrar(residuoRegistrar);
-        }catch(PersistenciaException e){
+        } catch (PersistenciaException e) {
+            // Propaga PersistenciaException como BOException
             throw new BOException(e.getMessage(), e);
-        }catch(ValidacionesException a){
+        } catch (ValidacionesException a) {
+            // Propaga ValidacionesException como BOException
             throw new BOException(a.getMessage(), a);
         }
     }
-    
-    public Residuo buscar(int idResiduo) throws BOException{
-        try{
+
+    /**
+     * Busca un residuo por su ID.
+     * 
+     * @param idResiduo El ID del residuo a buscar.
+     * @return El Residuo encontrado.
+     * @throws BOException Si hay un problema durante el proceso de búsqueda, encapsulando
+     *                     tanto PersistenciaException como ValidacionesException.
+     */
+    public Residuo buscar(int idResiduo) throws BOException {
+        try {
+            // Valida el ID antes de buscar el Residuo
             this.validaBuscarResiduo(idResiduo);
+            
+            // Busca el Residuo utilizando la capa de persistencia
             return persistencia.buscarResiduo(idResiduo);
-        }catch(PersistenciaException | ValidacionesException e){
+        } catch (PersistenciaException | ValidacionesException e) {
+            // Propaga PersistenciaException o ValidacionesException como BOException
             throw new BOException(e.getMessage(), e);
         }
     }
-    
-    private void validarRegistrarResiduo(Residuo residuoValidar) throws ValidacionesException{
-        if(residuoValidar == null){
-            throw new ValidacionesException("Residuo a registrar es nulo / No hay informacion");
+
+    /**
+     * Valida el objeto Residuo antes del registro.
+     * 
+     * @param residuoValidar El objeto Residuo a validar.
+     * @throws ValidacionesException Si la validación falla, indicando que el objeto Residuo
+     *                              es nulo o carece de información necesaria.
+     */
+    private void validarRegistrarResiduo(Residuo residuoValidar) throws ValidacionesException {
+        if (residuoValidar == null) {
+            throw new ValidacionesException("Residuo a registrar es nulo / No hay información");
         }
     }
-    
-    private void validaBuscarResiduo(int residuoValidar) throws ValidacionesException{
-        if(residuoValidar <= 0){
+
+    /**
+     * Valida el ID del Residuo antes de la búsqueda.
+     * 
+     * @param residuoValidar El ID del Residuo a validar.
+     * @throws ValidacionesException Si la validación falla, indicando que el ID es menor o igual a 0.
+     */
+    private void validaBuscarResiduo(int residuoValidar) throws ValidacionesException {
+        if (residuoValidar <= 0) {
             throw new ValidacionesException("ID del residuo es menor o igual a 0");
         }
     }
