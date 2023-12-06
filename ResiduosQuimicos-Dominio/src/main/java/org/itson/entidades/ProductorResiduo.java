497591dc5af685cd186799fa1c0bed6a06f67775
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:27:08 -0600

    ProductorResiduo.java

--- ResiduosQuimicos-Dominio/src/main/java/org/itson/entidades/ProductorResiduo.java
+++ ResiduosQuimicos-Dominio/src/main/java/org/itson/entidades/ProductorResiduo.java
@@ -18,60 +18,114 @@ import javax.persistence.Table;
 /**
  *
  * @author TADEO
+ *
+ * Clase que representa la entidad de la relación entre un productor y un residuo.
+ * 
+ * La entidad está mapeada a una tabla llamada "productores_residuos" en la base de datos.
  */
 @Entity
 @Table(name = "productores_residuos")
 public class ProductorResiduo implements Serializable {
 
+    /** Identificador único de la relación entre productor y residuo. */
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /** Relación Many-to-One con la entidad Productor. */
     @ManyToOne
     @JoinColumn(name = "idProductor", nullable = true)
     private Productor productor;
 
+    /** Relación Many-to-One con la entidad Residuo. */
     @ManyToOne
     @JoinColumn(name = "idResiduo")
     private Residuo residuo;
 
+    /** Constructor por defecto. */
     public ProductorResiduo() {
     }
 
+    /**
+     * Constructor que toma el identificador como parámetro.
+     * 
+     * @param id El identificador único de la relación.
+     */
     public ProductorResiduo(Long id) {
         this.id = id;
     }
 
+    /**
+     * Constructor que toma un productor y un residuo como parámetros.
+     * 
+     * @param productor El productor asociado a la relación.
+     * @param residuo   El residuo asociado a la relación.
+     */
     public ProductorResiduo(Productor productor, Residuo residuo) {
         this.productor = productor;
         this.residuo = residuo;
     }
 
+    /**
+     * Obtiene el identificador único de la relación.
+     * 
+     * @return El ID de la relación.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Establece el identificador único de la relación.
+     * 
+     * @param id El nuevo ID de la relación.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Obtiene el productor asociado a la relación.
+     * 
+     * @return El productor asociado.
+     */
     public Productor getProductor() {
         return productor;
     }
 
+    /**
+     * Establece el productor asociado a la relación.
+     * 
+     * @param productor El nuevo productor asociado.
+     */
     public void setProductor(Productor productor) {
         this.productor = productor;
     }
 
+    /**
+     * Obtiene el residuo asociado a la relación.
+     * 
+     * @return El residuo asociado.
+     */
     public Residuo getResiduo() {
         return residuo;
     }
 
+    /**
+     * Establece el residuo asociado a la relación.
+     * 
+     * @param residuo El nuevo residuo asociado.
+     */
     public void setResiduo(Residuo residuo) {
         this.residuo = residuo;
     }
 
+    /**
+     * Genera un código hash único para la relación entre productor y residuo.
+     * 
+     * @return El código hash.
+     */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -79,19 +133,26 @@ public class ProductorResiduo implements Serializable {
         return hash;
     }
 
+    /**
+     * Compara si dos relaciones entre productor y residuo son iguales.
+     * 
+     * @param object El objeto a comparar.
+     * @return `true` si las relaciones son iguales, `false` en caso contrario.
+     */
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof ProductorResiduo)) {
             return false;
         }
         ProductorResiduo other = (ProductorResiduo) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+        return (this.id != null || other.id == null) && (this.id == null || this.id.equals(other.id));
     }
 
+    /**
+     * Obtiene una representación en cadena de la relación entre productor y residuo.
+     * 
+     * @return La representación en cadena.
+     */
     @Override
     public String toString() {
         return "ProductorResiduo{" + "id=" + id + ", productor=" + productor + ", residuo=" + residuo + '}';
