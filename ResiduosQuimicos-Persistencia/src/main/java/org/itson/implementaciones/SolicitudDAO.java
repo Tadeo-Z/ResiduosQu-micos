3e107ca7abde2a022a51d48f16717ae451c3c7af
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:53:01 -0600

    SolicitudDAO.java

--- ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/SolicitudDAO.java
+++ ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/SolicitudDAO.java
@@ -14,43 +14,68 @@ import org.itson.interfaces.iSolicitud;
 /**
  *
  * @author TADEO
+ * Clase que implementa la interfaz iSolicitud y se encarga de realizar operaciones de persistencia
+ * relacionadas con la entidad Solicitud.
  */
-public class SolicitudDAO implements iSolicitud{
+public class SolicitudDAO implements iSolicitud {
 
+    /**
+     * Factoría de Entity Manager para gestionar las entidades de persistencia.
+     */
     EntityManagerFactory emf;
-    
-    public SolicitudDAO(){
+
+    /**
+     * Constructor de la clase que inicializa la factoría de Entity Manager.
+     */
+    public SolicitudDAO() {
         this.emf = Conexion.getConexion();
     }
-    
+
+    /**
+     * Registra una nueva Solicitud en la base de datos.
+     *
+     * @param solicitudRegistrar La Solicitud a registrar.
+     * @return La Solicitud registrada.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
     @Override
     public Solicitud registrar(Solicitud solicitudRegistrar) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             em.persist(solicitudRegistrar);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return solicitudRegistrar;
     }
-    
-    public Solicitud buscar(int idSolicitud) throws PersistenciaException{
+
+    /**
+     * Busca una Solicitud en la base de datos por su identificador.
+     *
+     * @param idSolicitud El identificador de la Solicitud a buscar.
+     * @return La Solicitud encontrada o null si no se encuentra.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
+    public Solicitud buscar(int idSolicitud) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
         Solicitud solicitudBuscar = null;
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             solicitudBuscar = em.find(Solicitud.class, idSolicitud);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return solicitudBuscar;
     }
-    
 }
