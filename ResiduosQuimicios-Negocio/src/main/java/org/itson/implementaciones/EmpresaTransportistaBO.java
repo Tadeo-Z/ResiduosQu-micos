fb841ff5d3d44751368f71e11e9b862b1b7eadfb
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:01:02 -0600

    EmpresaTransportistaBO.java

--- ResiduosQuimicios-Negocio/src/main/java/org/itson/implementaciones/EmpresaTransportistaBO.java
+++ ResiduosQuimicios-Negocio/src/main/java/org/itson/implementaciones/EmpresaTransportistaBO.java
@@ -13,48 +13,95 @@ import org.itson.interfaces.iEmpresaTransportistaBO;
 /**
  *
  * @author TADEO
+ *
+ * Clase que representa la lógica de negocio (BO) para la gestión de empresas transportistas.
+ * Implementa la interfaz iEmpresaTransportistaBO.
  */
-public class EmpresaTransportistaBO implements iEmpresaTransportistaBO{
+public class EmpresaTransportistaBO implements iEmpresaTransportistaBO {
 
+    /**
+     * Objeto de la clase FachadaPersistencia utilizado para interactuar con la capa de persistencia.
+     */
     FachadaPersistencia persistencia;
-    
-    public EmpresaTransportistaBO(){
+
+    /**
+     * Constructor predeterminado que inicializa el objeto FachadaPersistencia.
+     */
+    public EmpresaTransportistaBO() {
         persistencia = new FachadaPersistencia();
     }
+}
     
-    @Override
-    public EmpresaTransportista registrar(EmpresaTransportista empresaTransportistaRegistrar) throws BOException {
-        try{
-            this.validarRegistrarEmpresaTransportista(empresaTransportistaRegistrar);
-            return persistencia.registrar(empresaTransportistaRegistrar);
-        }catch(PersistenciaException e){
-            throw new BOException(e.getMessage(), e);
-        }catch(ValidacionesException a){
-            throw new BOException(a.getMessage(), a);
-        }
+/**
+ * Registra una nueva EmpresaTransportista.
+ * 
+ * @param empresaTransportistaRegistrar El objeto EmpresaTransportista que se registrará.
+ * @return La EmpresaTransportista registrada.
+ * @throws BOException Si hay un problema durante el proceso de registro, encapsulando
+ *                     tanto PersistenciaException como ValidacionesException.
+ */
+@Override
+public EmpresaTransportista registrar(EmpresaTransportista empresaTransportistaRegistrar) throws BOException {
+    try {
+        // Valida la EmpresaTransportista antes del registro
+        this.validarRegistrarEmpresaTransportista(empresaTransportistaRegistrar);
+        
+        // Registra la EmpresaTransportista utilizando la capa de persistencia
+        return persistencia.registrar(empresaTransportistaRegistrar);
+    } catch (PersistenciaException e) {
+        // Propaga PersistenciaException como BOException
+        throw new BOException(e.getMessage(), e);
+    } catch (ValidacionesException a) {
+        // Propaga ValidacionesException como BOException
+        throw new BOException(a.getMessage(), a);
     }
+}
     
-    public EmpresaTransportista buscar(int idEmpresaTransportista) throws BOException{
-        try{
-            this.validarBuscarEmpresaTransportista(idEmpresaTransportista);
-            return persistencia.buscarEmpresaTransportista(idEmpresaTransportista);
-        }catch(PersistenciaException e){
-            throw new BOException(e.getMessage(), e);
-        }catch(ValidacionesException a){
-            throw new BOException(a.getMessage(), a);
-        }
+/**
+ * Busca una EmpresaTransportista según el ID proporcionado.
+ * 
+ * @param idEmpresaTransportista El ID de la EmpresaTransportista a buscar.
+ * @return La EmpresaTransportista encontrada.
+ * @throws BOException Si hay un problema durante el proceso de búsqueda, encapsulando
+ *                     tanto PersistenciaException como ValidacionesException.
+ */
+public EmpresaTransportista buscar(int idEmpresaTransportista) throws BOException {
+    try {
+        // Valida el ID antes de buscar la EmpresaTransportista
+        this.validarBuscarEmpresaTransportista(idEmpresaTransportista);
+        
+        // Busca la EmpresaTransportista utilizando la capa de persistencia
+        return persistencia.buscarEmpresaTransportista(idEmpresaTransportista);
+    } catch (PersistenciaException e) {
+        // Propaga PersistenciaException como BOException
+        throw new BOException(e.getMessage(), e);
+    } catch (ValidacionesException a) {
+        // Propaga ValidacionesException como BOException
+        throw new BOException(a.getMessage(), a);
     }
+}
     
-    private void validarRegistrarEmpresaTransportista(EmpresaTransportista empresaTransportistaValidar) throws ValidacionesException{
-        if(empresaTransportistaValidar == null){
-            throw new ValidacionesException("Empresa transportista a insertar es nulo / No hay informacion");
-        }
+/**
+ * Valida el objeto EmpresaTransportista antes del registro.
+ * 
+ * @param empresaTransportistaValidar El objeto EmpresaTransportista a validar.
+ * @throws ValidacionesException Si la validación falla, indicando que el objeto EmpresaTransportista
+ *                              es nulo o carece de información necesaria.
+ */
+private void validarRegistrarEmpresaTransportista(EmpresaTransportista empresaTransportistaValidar) throws ValidacionesException {
+    if (empresaTransportistaValidar == null) {
+        throw new ValidacionesException("Empresa transportista a insertar es nulo / No hay información");
     }
+}
     
-    private void validarBuscarEmpresaTransportista(int empresaTransportistaValidar) throws ValidacionesException{
-        if(empresaTransportistaValidar <= 0){
-            throw new ValidacionesException("ID de la empresa transportista es menor o igual a 0");
-        }
+/**
+ * Valida el ID de la EmpresaTransportista antes de la búsqueda.
+ * 
+ * @param empresaTransportistaValidar El ID de la EmpresaTransportista a validar.
+ * @throws ValidacionesException Si la validación falla, indicando que el ID es menor o igual a 0.
+ */
+private void validarBuscarEmpresaTransportista(int empresaTransportistaValidar) throws ValidacionesException {
+    if (empresaTransportistaValidar <= 0) {
+        throw new ValidacionesException("ID de la empresa transportista es menor o igual a 0");
     }
-    
 }
