2b708c78adbada1869925399b6d4ff1a26587216
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:18:55 -0600

    iResiduoSolicitudBO.java

--- ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iResiduoSolicitudBO.java
+++ ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iResiduoSolicitudBO.java
@@ -10,10 +10,31 @@ import org.itson.excepciones.BOException;
 /**
  *
  * @author TADEO
+*
+ * Interfaz que define las operaciones de lógica de negocio (BO) para la gestión de solicitudes de residuos.
+ * 
+ * Las clases que implementen esta interfaz deben proporcionar implementaciones para registrar
+ * y buscar información relacionada con solicitudes de residuos.
  */
 public interface iResiduoSolicitudBO {
-    
+
+    /**
+     * Registra una nueva solicitud de residuos.
+     * 
+     * @param residuoSolicitudRegistrar La ResiduoSolicitud que se registrará.
+     * @return La ResiduoSolicitud registrada.
+     * @throws BOException Si hay un problema durante el proceso de registro, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public ResiduoSolicitud registrar(ResiduoSolicitud residuoSolicitudRegistrar) throws BOException;
-    
+
+    /**
+     * Busca una solicitud de residuos por su ID.
+     * 
+     * @param idResiduoSolicitud El ID de la solicitud de residuos a buscar.
+     * @return La ResiduoSolicitud encontrada.
+     * @throws BOException Si hay un problema durante el proceso de búsqueda, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public ResiduoSolicitud buscar(int idResiduoSolicitud) throws BOException;
 }
