889bf4a06a3566723df74f9b741a65b5a9ee6cd0
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:21:08 -0600

    iTrasladoBO.java

--- ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iTrasladoBO.java
+++ ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iTrasladoBO.java
@@ -7,14 +7,32 @@ package org.itson.interfaces;
 import org.itson.entidades.Traslado;
 import org.itson.excepciones.BOException;
 
+
 /**
- *
- * @author TADEO
+ * Interfaz que define las operaciones de lógica de negocio (BO) para la gestión de traslados.
+ * 
+ * Las clases que implementen esta interfaz deben proporcionar implementaciones para registrar
+ * y buscar información relacionada con traslados.
  */
 public interface iTrasladoBO {
-    
+
+    /**
+     * Registra un nuevo traslado.
+     * 
+     * @param trasladoRegistrar El Traslado que se registrará.
+     * @return El Traslado registrado.
+     * @throws BOException Si hay un problema durante el proceso de registro, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public Traslado registrar(Traslado trasladoRegistrar) throws BOException;
-    
+
+    /**
+     * Busca un traslado por su ID.
+     * 
+     * @param idTraslado El ID del traslado a buscar.
+     * @return El Traslado encontrado.
+     * @throws BOException Si hay un problema durante el proceso de búsqueda, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public Traslado buscar(int idTraslado) throws BOException;
-    
 }
