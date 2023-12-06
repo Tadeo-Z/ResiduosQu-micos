4167daab9270d7cc9bf1fdb7abb40186eb45e17c
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:50:37 -0600

    QuimicoDAO.java

--- ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/QuimicoDAO.java
+++ ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/QuimicoDAO.java
@@ -14,43 +14,68 @@ import org.itson.interfaces.iQuimico;
 /**
  *
  * @author TADEO
+ * Clase que implementa la interfaz iQuimico y se encarga de realizar operaciones de persistencia
+ * relacionadas con la entidad Quimico.
  */
-public class QuimicoDAO implements iQuimico{
+public class QuimicoDAO implements iQuimico {
 
+    /**
+     * Factoría de Entity Manager para gestionar las entidades de persistencia.
+     */
     EntityManagerFactory emf;
-    
-    public QuimicoDAO(){
+
+    /**
+     * Constructor de la clase que inicializa la factoría de Entity Manager.
+     */
+    public QuimicoDAO() {
         this.emf = Conexion.getConexion();
     }
-    
+
+    /**
+     * Registra un nuevo Quimico en la base de datos.
+     *
+     * @param quimicoRegistrar El Quimico a registrar.
+     * @return El Quimico registrado.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
     @Override
     public Quimico registrar(Quimico quimicoRegistrar) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             em.persist(quimicoRegistrar);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return quimicoRegistrar;
     }
-    
-    public Quimico buscar(int idQuimico) throws PersistenciaException{
+
+    /**
+     * Busca un Quimico en la base de datos por su identificador.
+     *
+     * @param idQuimico El identificador del Quimico a buscar.
+     * @return El Quimico encontrado o null si no se encuentra.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
+    public Quimico buscar(int idQuimico) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
         Quimico quimicoBuscar = null;
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             quimicoBuscar = em.find(Quimico.class, idQuimico);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-             
+
         return quimicoBuscar;
     }
-    
 }
