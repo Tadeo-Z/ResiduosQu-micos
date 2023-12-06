63b31d49a793b35fd9afbec7ab09b2b019c6e0fb
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:38:43 -0600

    PersistenciaException.java

--- ResiduosQuimicos-Persistencia/src/main/java/org/itson/excepciones/PersistenciaException.java
+++ ResiduosQuimicos-Persistencia/src/main/java/org/itson/excepciones/PersistenciaException.java
@@ -7,18 +7,35 @@ package org.itson.excepciones;
 /**
  *
  * @author TADEO
+ * Excepción personalizada para manejar errores relacionados con la persistencia.
  */
-public class PersistenciaException extends Exception{
+public class PersistenciaException extends Exception {
 
+    /**
+     * Constructor predeterminado.
+     */
     public PersistenciaException() {
+        // Llama al constructor de la clase base (Exception) sin argumentos.
     }
 
+    /**
+     * Constructor que acepta un mensaje descriptivo del error.
+     *
+     * @param message Mensaje descriptivo del error.
+     */
     public PersistenciaException(String message) {
+        // Llama al constructor de la clase base (Exception) con el mensaje proporcionado.
         super(message);
     }
 
+    /**
+     * Constructor que acepta un mensaje descriptivo y una causa del error.
+     *
+     * @param message Mensaje descriptivo del error.
+     * @param cause   Causa del error.
+     */
     public PersistenciaException(String message, Throwable cause) {
+        // Llama al constructor de la clase base (Exception) con el mensaje y la causa proporcionados.
         super(message, cause);
     }
-    
 }
