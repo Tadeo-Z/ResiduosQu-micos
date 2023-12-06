062cbfeb92407cfe24e9775cdcd594fbaea5537f
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:48:52 -0600

    ProductorDAO.java

--- ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/ProductorDAO.java
+++ ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/ProductorDAO.java
@@ -14,57 +14,91 @@ import org.itson.interfaces.iProductor;
 /**
  *
  * @author TADEO
+ * Clase que implementa la interfaz iProductor y se encarga de realizar operaciones de persistencia
+ * relacionadas con la entidad Productor.
  */
-public class ProductorDAO implements iProductor{
+public class ProductorDAO implements iProductor {
 
+    /**
+     * Factoría de Entity Manager para gestionar las entidades de persistencia.
+     */
     EntityManagerFactory emf;
-    
-    public ProductorDAO(){
+
+    /**
+     * Constructor de la clase que inicializa la factoría de Entity Manager.
+     */
+    public ProductorDAO() {
         this.emf = Conexion.getConexion();
     }
-    
+
+    /**
+     * Registra un nuevo Productor en la base de datos.
+     *
+     * @param productorRegistrar El Productor a registrar.
+     * @return El Productor registrado.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
     @Override
     public Productor registrar(Productor productorRegistrar) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             em.persist(productorRegistrar);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return productorRegistrar;
     }
 
+    /**
+     * Actualiza la información de un Productor en la base de datos.
+     *
+     * @param productorActualizar El Productor con la información actualizada.
+     * @return El Productor actualizado.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
     @Override
     public Productor actualizar(Productor productorActualizar) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
-        try{
+        try {
             em.getTransaction().begin();
             em.merge(productorActualizar);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return productorActualizar;
     }
-    
-    public Productor buscar(int idProductor) throws PersistenciaException{
+
+    /**
+     * Busca un Productor en la base de datos por su identificador.
+     *
+     * @param idProductor El identificador del Productor a buscar.
+     * @return El Productor encontrado o null si no se encuentra.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
+    public Productor buscar(int idProductor) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
         Productor productorBuscar = null;
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             productorBuscar = em.find(Productor.class, idProductor);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return productorBuscar;
     }
-    
 }
