82b410c3392ed3cee9060b792a9e7951e70bcf41
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:55:25 -0600

    iEmpresaTransportista.java

--- ResiduosQuimicos-Persistencia/src/main/java/org/itson/interfaces/iEmpresaTransportista.java
+++ ResiduosQuimicos-Persistencia/src/main/java/org/itson/interfaces/iEmpresaTransportista.java
@@ -10,8 +10,25 @@ import org.itson.excepciones.PersistenciaException;
 /**
  *
  * @author TADEO
+ * Interfaz que define los métodos de persistencia relacionados con la entidad EmpresaTransportista.
  */
 public interface iEmpresaTransportista {
+    
+    /**
+     * Registra una nueva empresa transportista en la capa de persistencia.
+     *
+     * @param empresaTransportistaRegistrar La empresa transportista a registrar.
+     * @return La empresa transportista registrada.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
     public EmpresaTransportista registrar(EmpresaTransportista empresaTransportistaRegistrar) throws PersistenciaException;
+    
+    /**
+     * Busca una empresa transportista en la capa de persistencia por su identificador.
+     *
+     * @param idEmpresaTransportista El identificador de la empresa transportista a buscar.
+     * @return La empresa transportista encontrada o null si no se encuentra.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
     public EmpresaTransportista buscar(int idEmpresaTransportista) throws PersistenciaException;
 }
