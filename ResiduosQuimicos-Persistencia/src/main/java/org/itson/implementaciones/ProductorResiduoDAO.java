27ff12d1bc73e6ac7d0d59b2eb89a0598a1a3166
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:49:55 -0600

    ProductorResiduoDAO.java

--- ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/ProductorResiduoDAO.java
+++ ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/ProductorResiduoDAO.java
@@ -14,42 +14,68 @@ import org.itson.interfaces.iProductorResiduo;
 /**
  *
  * @author TADEO
+ * Clase que implementa la interfaz iProductorResiduo y se encarga de realizar operaciones de persistencia
+ * relacionadas con la entidad ProductorResiduo.
  */
-public class ProductorResiduoDAO implements iProductorResiduo{
+public class ProductorResiduoDAO implements iProductorResiduo {
 
+    /**
+     * Factoría de Entity Manager para gestionar las entidades de persistencia.
+     */
     EntityManagerFactory emf;
-    
-    public ProductorResiduoDAO(){
+
+    /**
+     * Constructor de la clase que inicializa la factoría de Entity Manager.
+     */
+    public ProductorResiduoDAO() {
         this.emf = Conexion.getConexion();
     }
+
+    /**
+     * Registra un nuevo ProductorResiduo en la base de datos.
+     *
+     * @param productorResiduoRegistrar El ProductorResiduo a registrar.
+     * @return El ProductorResiduo registrado.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
     @Override
     public ProductorResiduo registrar(ProductorResiduo productorResiduoRegistrar) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             em.persist(productorResiduoRegistrar);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return productorResiduoRegistrar;
     }
-    
-    public ProductorResiduo buscar(int idProductorResiduo) throws PersistenciaException{
+
+    /**
+     * Busca un ProductorResiduo en la base de datos por su identificador.
+     *
+     * @param idProductorResiduo El identificador del ProductorResiduo a buscar.
+     * @return El ProductorResiduo encontrado o null si no se encuentra.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
+    public ProductorResiduo buscar(int idProductorResiduo) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
         ProductorResiduo productorResiduoBuscar = null;
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             productorResiduoBuscar = em.find(ProductorResiduo.class, idProductorResiduo);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return productorResiduoBuscar;
     }
-    
 }
