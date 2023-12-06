c183c041c53a35f0d16f1ebc89635de1075cacc7
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:52:16 -0600

    ResiduoSolicitudDAO.java

--- ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/ResiduoSolicitudDAO.java
+++ ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/ResiduoSolicitudDAO.java
@@ -14,43 +14,68 @@ import org.itson.interfaces.iResiduoSolicitud;
 /**
  *
  * @author TADEO
+ * Clase que implementa la interfaz iResiduoSolicitud y se encarga de realizar operaciones de persistencia
+ * relacionadas con la entidad ResiduoSolicitud.
  */
-public class ResiduoSolicitudDAO implements iResiduoSolicitud{
+public class ResiduoSolicitudDAO implements iResiduoSolicitud {
 
+    /**
+     * Factoría de Entity Manager para gestionar las entidades de persistencia.
+     */
     EntityManagerFactory emf;
-    
-    public ResiduoSolicitudDAO(){
+
+    /**
+     * Constructor de la clase que inicializa la factoría de Entity Manager.
+     */
+    public ResiduoSolicitudDAO() {
         this.emf = Conexion.getConexion();
     }
-    
+
+    /**
+     * Registra un nuevo ResiduoSolicitud en la base de datos.
+     *
+     * @param residuoSolicitudRegistrar El ResiduoSolicitud a registrar.
+     * @return El ResiduoSolicitud registrado.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
     @Override
     public ResiduoSolicitud registrar(ResiduoSolicitud residuoSolicitudRegistrar) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             em.persist(residuoSolicitudRegistrar);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return residuoSolicitudRegistrar;
     }
-    
-    public ResiduoSolicitud buscar(int idResiduoSolicitud) throws PersistenciaException{
+
+    /**
+     * Busca un ResiduoSolicitud en la base de datos por su identificador.
+     *
+     * @param idResiduoSolicitud El identificador del ResiduoSolicitud a buscar.
+     * @return El ResiduoSolicitud encontrado o null si no se encuentra.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
+    public ResiduoSolicitud buscar(int idResiduoSolicitud) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
         ResiduoSolicitud residuoSolicitudBuscar = null;
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             residuoSolicitudBuscar = em.find(ResiduoSolicitud.class, idResiduoSolicitud);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return residuoSolicitudBuscar;
     }
-    
 }
