a89656ba15c786074cc184ad62018bd568919225
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:14:57 -0600

    iEmpresaTransportistaBO.java

--- ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iEmpresaTransportistaBO.java
+++ ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iEmpresaTransportistaBO.java
@@ -10,11 +10,31 @@ import org.itson.excepciones.BOException;
 /**
  *
  * @author TADEO
+ *
+ * Interfaz que define las operaciones de lógica de negocio (BO) para la gestión de empresas transportistas.
+ * 
+ * Las clases que implementen esta interfaz deben proporcionar implementaciones para registrar y buscar
+ * información relacionada con empresas transportistas.
  */
 public interface iEmpresaTransportistaBO {
-    
+
+    /**
+     * Registra una nueva empresa transportista.
+     * 
+     * @param empresaTransportistaRegistrar La EmpresaTransportista que se registrará.
+     * @return La EmpresaTransportista registrada.
+     * @throws BOException Si hay un problema durante el proceso de registro, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public EmpresaTransportista registrar(EmpresaTransportista empresaTransportistaRegistrar) throws BOException;
 
+    /**
+     * Busca una empresa transportista por su ID.
+     * 
+     * @param idEmpresaTransportista El ID de la empresa transportista a buscar.
+     * @return La EmpresaTransportista encontrada.
+     * @throws BOException Si hay un problema durante el proceso de búsqueda, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public EmpresaTransportista buscar(int idEmpresaTransportista) throws BOException;
-    
 }
