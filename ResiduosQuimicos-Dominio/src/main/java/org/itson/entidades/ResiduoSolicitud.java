62a76ee9ee1ad6e337202c729bc5a4ecf7f9b32b
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:32:12 -0600

    ResiduoSolicitud.java

--- ResiduosQuimicos-Dominio/src/main/java/org/itson/entidades/ResiduoSolicitud.java
+++ ResiduosQuimicos-Dominio/src/main/java/org/itson/entidades/ResiduoSolicitud.java
@@ -20,37 +20,59 @@ import org.itson.enumeradores.UnidadMedida;
 /**
  *
  * @author TADEO
+ * Clase que representa la entidad de relación entre Residuo y Solicitud.
+ * 
+ * La entidad está mapeada a una tabla llamada "residuos_solicitudes" en la base de datos.
  */
 @Entity
 @Table(name = "residuos_solicitudes")
 public class ResiduoSolicitud implements Serializable {
 
+    /** Identificador único de la relación residuo-solicitud. */
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /** Relación Many-to-One con la entidad Residuo. */
     @ManyToOne
     @JoinColumn(name = "idResiduo")
     private Residuo residuo;
     
+    /** Cantidad del residuo en la solicitud. */
     @Column(name = "cantidad")
     private int cantidad;
     
+    /** Unidad de medida para la cantidad del residuo en la solicitud. */
     @Column(name = "unidadMedida")
     @Enumerated(EnumType.STRING)
     private UnidadMedida unidadMedida;
 
+    /** Relación Many-to-One con la entidad Solicitud. */
     @ManyToOne()
     @JoinColumn(name = "id_solicitud", nullable = true)
     private Solicitud solicitud;
     
+    /** Constructor por defecto. */
     public ResiduoSolicitud() {}
 
+    /**
+     * Constructor que toma un residuo como parámetro.
+     * 
+     * @param residuo El residuo asociado a la relación.
+     */
     public ResiduoSolicitud(Residuo residuo) {
         this.residuo = residuo;
     }
 
+    /**
+     * Constructor que toma un residuo, cantidad, unidad de medida y solicitud como parámetros.
+     * 
+     * @param residuo       El residuo asociado a la relación.
+     * @param cantidad      La cantidad del residuo en la solicitud.
+     * @param unidadMedida  La unidad de medida para la cantidad.
+     * @param solicitud     La solicitud asociada a la relación.
+     */
     public ResiduoSolicitud(Residuo residuo, int cantidad, UnidadMedida unidadMedida, Solicitud solicitud) {
         this.residuo = residuo;
         this.cantidad = cantidad;
@@ -58,46 +80,101 @@ public class ResiduoSolicitud implements Serializable {
         this.solicitud = solicitud;
     }
     
+    /**
+     * Obtiene el identificador único de la relación.
+     * 
+     * @return El ID de la relación.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Establece el identificador único de la relación.
+     * 
+     * @param id El nuevo ID de la relación.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Obtiene el residuo asociado a la relación.
+     * 
+     * @return El residuo asociado.
+     */
     public Residuo getResiduo() {
         return residuo;
     }
 
+    /**
+     * Establece el residuo asociado a la relación.
+     * 
+     * @param residuo El nuevo residuo asociado.
+     */
     public void setResiduo(Residuo residuo) {
         this.residuo = residuo;
     }
 
+    /**
+     * Obtiene la cantidad del residuo en la solicitud.
+     * 
+     * @return La cantidad del residuo.
+     */
     public int getCantidad() {
         return cantidad;
     }
 
+    /**
+     * Establece la cantidad del residuo en la solicitud.
+     * 
+     * @param cantidad La nueva cantidad del residuo.
+     */
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
 
+    /**
+     * Obtiene la unidad de medida para la cantidad del residuo.
+     * 
+     * @return La unidad de medida.
+     */
     public UnidadMedida getUnidadMedida() {
         return unidadMedida;
     }
 
+    /**
+     * Establece la unidad de medida para la cantidad del residuo.
+     * 
+     * @param unidadMedida La nueva unidad de medida.
+     */
     public void setUnidadMedida(UnidadMedida unidadMedida) {
         this.unidadMedida = unidadMedida;
     }
 
+    /**
+     * Obtiene la solicitud asociada a la relación.
+     * 
+     * @return La solicitud asociada.
+     */
     public Solicitud getSolicitud() {
         return solicitud;
     }
 
+    /**
+     * Establece la solicitud asociada a la relación.
+     * 
+     * @param solicitud La nueva solicitud asociada.
+     */
     public void setSolicitud(Solicitud solicitud) {
         this.solicitud = solicitud;
     }
     
+    /**
+     * Genera un código hash único para la relación residuo-solicitud.
+     * 
+     * @return El código hash.
+     */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -105,22 +182,35 @@ public class ResiduoSolicitud implements Serializable {
         return hash;
     }
 
+    /**
+     * Compara si dos relaciones residuo-solicitud son iguales.
+     * 
+     * @param object El objeto a comparar.
+     * @return `true` si las relaciones son iguales, `false` en caso contrario.
+     */
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof ResiduoSolicitud)) {
             return false;
         }
         ResiduoSolicitud other = (ResiduoSolicitud) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+        return (this.id == null && other.id == null) || (this.id != null && this.id.equals(other.id));
     }
 
+    /**
+     * Obtiene una representación en cadena de la relación residuo-solicitud.
+     * 
+     * @return La representación en cadena.
+     */
     @Override
     public String toString() {
-        return "ResiduoSolicitud{" + "id=" + id + ", residuo=" + residuo + ", cantidad=" + cantidad + ", unidadMedida=" + unidadMedida + ", solicitud=" + solicitud + '}';
+        return "ResiduoSolicitud{" +
+               "id=" + id +
+               ", residuo=" + residuo +
+               ", cantidad=" + cantidad +
+               ", unidadMedida=" + unidadMedida +
+               ", solicitud=" + solicitud +
+               '}';
     }
     
 }
