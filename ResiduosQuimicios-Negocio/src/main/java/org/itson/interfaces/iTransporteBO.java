2ce856127fb101dd773f83c230d46ec5de639d14
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:20:29 -0600

    iTransporteBO.java

--- ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iTransporteBO.java
+++ ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iTransporteBO.java
@@ -8,13 +8,30 @@ import org.itson.entidades.Transporte;
 import org.itson.excepciones.BOException;
 
 /**
- *
- * @author TADEO
+ * Interfaz que define las operaciones de lógica de negocio (BO) para la gestión de transportes.
+ * 
+ * Las clases que implementen esta interfaz deben proporcionar implementaciones para registrar
+ * y buscar información relacionada con transportes.
  */
 public interface iTransporteBO {
-    
+
+    /**
+     * Registra un nuevo transporte.
+     * 
+     * @param transporteRegistrar El Transporte que se registrará.
+     * @return El Transporte registrado.
+     * @throws BOException Si hay un problema durante el proceso de registro, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public Transporte registrar(Transporte transporteRegistrar) throws BOException;
-    
+
+    /**
+     * Busca un transporte por su ID.
+     * 
+     * @param idTransporte El ID del transporte a buscar.
+     * @return El Transporte encontrado.
+     * @throws BOException Si hay un problema durante el proceso de búsqueda, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public Transporte buscar(int idTransporte) throws BOException;
-    
 }
