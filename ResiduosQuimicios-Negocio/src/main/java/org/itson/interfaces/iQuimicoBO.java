da6e34d5a59a24b9ee909ba04c13a7617adea478
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:17:14 -0600

    iQuimicoBO.java

--- ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iQuimicoBO.java
+++ ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iQuimicoBO.java
@@ -10,10 +10,31 @@ import org.itson.excepciones.BOException;
 /**
  *
  * @author TADEO
+ *
+ * Interfaz que define las operaciones de lógica de negocio (BO) para la gestión de productos químicos.
+ * 
+ * Las clases que implementen esta interfaz deben proporcionar implementaciones para registrar
+ * y buscar información relacionada con productos químicos.
  */
 public interface iQuimicoBO {
-    
+
+    /**
+     * Registra un nuevo producto químico.
+     * 
+     * @param quimicoRegistrar El Quimico que se registrará.
+     * @return El Quimico registrado.
+     * @throws BOException Si hay un problema durante el proceso de registro, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public Quimico registrar(Quimico quimicoRegistrar) throws BOException;
-    
+
+    /**
+     * Busca un producto químico por su ID.
+     * 
+     * @param idQuimico El ID del producto químico a buscar.
+     * @return El Quimico encontrado.
+     * @throws BOException Si hay un problema durante el proceso de búsqueda, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public Quimico buscar(int idQuimico) throws BOException;
 }
