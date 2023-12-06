33a2789b386391affd0c8aeb8176582ecb2ac704
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:16:27 -0600

    iProductorResiduoBO.java

--- ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iProductorResiduoBO.java
+++ ResiduosQuimicios-Negocio/src/main/java/org/itson/interfaces/iProductorResiduoBO.java
@@ -10,10 +10,31 @@ import org.itson.excepciones.BOException;
 /**
  *
  * @author TADEO
+ *
+ * Interfaz que define las operaciones de lógica de negocio (BO) para la gestión de productores de residuos.
+ * 
+ * Las clases que implementen esta interfaz deben proporcionar implementaciones para registrar
+ * y buscar información relacionada con productores de residuos.
  */
 public interface iProductorResiduoBO {
-    
+
+    /**
+     * Registra un nuevo productor de residuos.
+     * 
+     * @param productorResiduoRegistrar El ProductorResiduo que se registrará.
+     * @return El ProductorResiduo registrado.
+     * @throws BOException Si hay un problema durante el proceso de registro, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public ProductorResiduo registrar(ProductorResiduo productorResiduoRegistrar) throws BOException;
-    
+
+    /**
+     * Busca un productor de residuos por su ID.
+     * 
+     * @param idProductorResiduo El ID del productor de residuos a buscar.
+     * @return El ProductorResiduo encontrado.
+     * @throws BOException Si hay un problema durante el proceso de búsqueda, encapsulando
+     *                     posibles excepciones específicas.
+     */
     public ProductorResiduo buscar(int idProductorResiduo) throws BOException;
 }
