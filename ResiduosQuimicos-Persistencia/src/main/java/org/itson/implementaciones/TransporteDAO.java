b552567ba5bbf7adf6281255bb203e0f08aa15fd
Paraguacho <133551791+Paraguacho@users.noreply.github.com>
2023-12-06 09:53:47 -0600

    TransporteDAO.java

--- ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/TransporteDAO.java
+++ ResiduosQuimicos-Persistencia/src/main/java/org/itson/implementaciones/TransporteDAO.java
@@ -14,43 +14,68 @@ import org.itson.interfaces.iTransporte;
 /**
  *
  * @author TADEO
+ * Clase que implementa la interfaz iTransporte y se encarga de realizar operaciones de persistencia
+ * relacionadas con la entidad Transporte.
  */
-public class TransporteDAO implements iTransporte{
+public class TransporteDAO implements iTransporte {
 
+    /**
+     * Factoría de Entity Manager para gestionar las entidades de persistencia.
+     */
     EntityManagerFactory emf;
-    
-    public TransporteDAO(){
+
+    /**
+     * Constructor de la clase que inicializa la factoría de Entity Manager.
+     */
+    public TransporteDAO() {
         this.emf = Conexion.getConexion();
     }
-    
+
+    /**
+     * Registra un nuevo Transporte en la base de datos.
+     *
+     * @param transporteRegistrar El Transporte a registrar.
+     * @return El Transporte registrado.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
     @Override
     public Transporte registrar(Transporte transporteRegistrar) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             em.persist(transporteRegistrar);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return transporteRegistrar;
     }
-    
-    public Transporte buscar(int idTransporte) throws PersistenciaException{
+
+    /**
+     * Busca un Transporte en la base de datos por su identificador.
+     *
+     * @param idTransporte El identificador del Transporte a buscar.
+     * @return El Transporte encontrado o null si no se encuentra.
+     * @throws PersistenciaException Si hay un error en la capa de persistencia.
+     */
+    public Transporte buscar(int idTransporte) throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
         Transporte transporteBuscar = null;
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             transporteBuscar = em.find(Transporte.class, idTransporte);
             em.getTransaction().commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return transporteBuscar;
     }
-    
 }
